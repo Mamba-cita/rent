ce04b884bf65ce473218834af77e1fe00084c0fd
Mamba-cita <EDWINSJOHN52@gmail.com>
2024-10-12 19:13:45 +0300

    authProvider function

--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -41,3 +41,30 @@ function authReducer(state, action) {
             return state;
     }
 }
+
+function AuthProvider(props) {
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
+   const login = (userData) => {
+       localStorage.setItem('token', userData.token);
+       dispatch({
+           type: 'LOGIN',
+           payload: userData,
+       });
+   };
+
+   function logout() {
+       localStorage.removeItem('token');
+       dispatch({ type: 'LOGOUT' });
+   };
+
+    return (
+         <AuthContext.Provider
+          value={{ user: state.user, login, logout }}
+          {...props}
+         />
+    );
+}
+
+
+export { AuthContext, AuthProvider };
